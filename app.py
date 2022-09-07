95f1e18c0540201dede646c7f9de2c5e5961a8da
Sahil1728 <shifusahil2811@gmail.com>
2022-09-07 21:51:08 +0530

    Created app.py file along with its basic code

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# registering the route
+@app.route("/")
+def hello_world():
+    return "Hello World"
+
+print("__name__")
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
